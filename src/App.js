f71040ae5d26266a77006c00e89989bccd5221a1
Jose Marquez <jmarquezv@gmail.com>
2023-06-07 02:11:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BaseColaboradores } from "./Components/BaseColaboradores/BaseColaboradores";
+import React, { useState } from "react";
 
 function App() {
+  const [inputSearch, setInputSearch] = useState("");
+  const [inputColab, setInputColab] = useState("");
+  const [inputMail, setInputMail] = useState("");
+  const [listColaboradores, setListColaboradores] = useState(BaseColaboradores);
+  console.log(inputSearch.value);
+
+  const colab = {
+    id: listColaboradores.length + 1,
+    nombre: inputColab,
+    correo: inputMail,
+  };
+
+  const agregarColaborador = () => {
+    setListColaboradores([...listColaboradores, colab]);
+  };
+  console.log(inputSearch);
+  let result = [];
+  if (!inputSearch) {
+    result = listColaboradores;
+  } else {
+    result = listColaboradores.filter((dato) =>
+      dato.nombre.includes(inputSearch)
+    );
+    console.log(result.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="buscador">
+        <h4>Buscar Colaborador</h4>
+        <input
+          id="input1"
+          type="text"
+          placeholder="Buscar colaborador"
+          onChange={(e) => setInputSearch(e.target.value)}
+        />
+      </div>
+      <div className="colaborador">
+        <h5>Nombre del Colaborador</h5>
+        <input
+          className="input2"
+          type="text"
+          placeholder="Ingresar nombre"
+          onChange={(e) => setInputColab(e.target.value)}
+        />
+      </div>
+      <div className="colaborador">
+        <h5>Correo del Colaborador</h5>
+        <input
+          className="input2"
+          type="text"
+          placeholder="Ingresar E-mail"
+          onChange={(e) => setInputMail(e.target.value)}
+        />
+      </div>
+      <div className="button1">
+        <button onClick={() => agregarColaborador()}>
+          Agregar Colaborador
+        </button>
+      </div>
+
+      <div className="resultados">
+        <ul >
+          {result.map((colaborador, i) => {
+            return (
+              <li  key={i} className="resultados1">
+                {colaborador.nombre} <span> - </span>
+                {colaborador.correo}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
